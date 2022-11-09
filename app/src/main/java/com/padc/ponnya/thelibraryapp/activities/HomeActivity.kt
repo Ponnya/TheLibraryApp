5595ff1b51b27b53dda6703cf63b2824be06e287
Ponnya <mr.ponnya@gmail.com>
2022-11-09 14:30:14 +0630

    -Add a search box in Home screen

--- app/src/main/java/com/padc/ponnya/thelibraryapp/activities/HomeActivity.kt
+++ app/src/main/java/com/padc/ponnya/thelibraryapp/activities/HomeActivity.kt
@@ -2,14 +2,13 @@ package com.padc.ponnya.thelibraryapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.padc.ponnya.thelibraryapp.R
-import com.padc.ponnya.thelibraryapp.databinding.ActivityMainBinding
+import com.padc.ponnya.thelibraryapp.databinding.ActivityHomeBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
