0abb187af1d7d2c420c91d946a7bd6931de85ebe
Ponnya <mr.ponnya@gmail.com>
2022-11-10 15:35:06 +0630

    -Add TabLayout and NavigationBar in Home Screen
    Signed-off-by: Ponnya <mr.ponnya@gmail.com>

---
+++ app/src/main/java/com/padc/ponnya/thelibraryapp/views/components/StraightProgressBar.kt
@@ -0,0 +1,54 @@
+package com.padc.ponnya.thelibraryapp.views.components
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.View
+import androidx.core.content.withStyledAttributes
+import com.padc.ponnya.thelibraryapp.R
+
+class StraightProgressBar @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    private val path = Path()
+
+    private val paint = Paint().apply {
+        style = Paint.Style.FILL
+
+    }
+
+    private var progress: Int = 0
+    private var mBackgroundColor = Color.GRAY
+    private var progressBarColor = Color.BLACK
+
+    init {
+        context.withStyledAttributes(attrs, R.styleable.StraightProgressBar){
+            progress = getInt(R.styleable.StraightProgressBar_progressNumber,progress)
+            mBackgroundColor = getColor(R.styleable.StraightProgressBar_backgroundLineColor,mBackgroundColor)
+            progressBarColor = getColor(R.styleable.StraightProgressBar_progressBarColor,progressBarColor)
+        }
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        paint.strokeWidth = height.toFloat()
+        drawInnerLine(canvas)
+        drawOuterLine(canvas)
+        super.onDraw(canvas)
+
+    }
+
+    private fun drawInnerLine(canvas: Canvas?) {
+        paint.color = mBackgroundColor
+        canvas?.drawLine(0f, height/2f, width.toFloat(), height/2f, paint)
+    }
+
+    private fun drawOuterLine(canvas: Canvas?) {
+        paint.color = progressBarColor
+        canvas?.drawLine(0f, height/2f, width * progress/100f, height/2f, paint)
+
+    }
+}
